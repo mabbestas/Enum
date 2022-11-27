7c7e8d3c3bb52dac3119be6e259dea8babcd8110
Burak Bestas <mabbestas@gmail.com>
2022-11-27 16:48:55 +0300

    Enum added

---
+++ Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 24;
+
+            if( sicaklik < (int)HavaDurumu.Normal)
+                Console.WriteLine("Hava çok soğuk!");
+            else if(sicaklik >= (int)HavaDurumu.Sicak)
+                Console.WriteLine("Hava çok soğuk!");
+            else if(sicaklik < (int)HavaDurumu.Sicak && sicaklik > (int)HavaDurumu.Soguk)
+                Console.WriteLine("Hava güzel!");
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi =1,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
